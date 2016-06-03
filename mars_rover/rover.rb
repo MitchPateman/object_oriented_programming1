bb5500f0cd846bb0d76fd1e39f70cee37689cb4f
Mitch Pateman <mitch_pateman@hotmail.com>
2016-06-03 13:41:07 -0400

    Added turn_left, turn_right, read_instuctions

--- mars_rover/rover.rb
+++ mars_rover/rover.rb
@@ -1,5 +1,5 @@
 class Rover
-	attr_accessor :rover_name, :x, :y, :direction, :move, :turn
+	#attr_accessor :rover_name, :x, :y, :direction, :move, :turn
 
 
 	def initialize
@@ -8,23 +8,44 @@ class Rover
 		@direction = @position[2].upcase.to_sym
 	end
 
-	def turn
-		case @turn   ####what is this
-			when "l" && @direction == :N then @direction = :W
-			when "r" && @direction == :N then @direction = :E
+	def read_instruction
+		@instructions.each do |command|
+			case @command
+				when "m" then move
+				when "l" then turn_left
+				when "r" then turn_right
+			end #End of read_instruction case
+		end # End of each do
+	end # End of read_instruction method
 
-			when "l" && @direction == :E then @direction = :N
-			when "r" && @direction == :E then @direction = :S
-
-			when "l" && @direction == :S then @direction = :E
-			when "r" && @direction == :S then @direction = :W
-
-			when "l" && @direction == :N then @direction = :W
-			when "r" && @direction == :N then @direction = :E
-
+	def turn_left
+		case @direction ####what is this varilable????
+			when :N then @direction = :W
+			when :E then @direction = :N
+			when :S then @direction = :E
+			when :N then @direction = :W
 			else "Enter l or r"
-				
-		end #### End of turn case
-	end ###End of Turn method
+		end #### End of turn left method
+	end
+
+	def turn_right
+			case @turn   ####what is this varilable????
+				when :N then @direction = :E
+				when :E then @direction = :S
+				when :S then @direction = :W
+				when :N then @direction = :E
+				else "Enter l or r"
+			end #### End of turn right case
+	end ###End of Turn right method
+
+
+	def move
+		case @direction
+			when :N then @y += 1
+			when :E then @x += 1
+			when :S then @y -= 1
+			when :W then @x -= 1
+		end #End of move case
+	end #End of move method
 
 end #End of Rover class
