559559b4d672dc74d5aef157824d0a0e95c75c49
Mitch Pateman <mitch_pateman@hotmail.com>
2016-06-03 15:45:46 -0400

    Working Nowgit status! must enter capital letters in input

--- mars_rover/mission_control.rb
+++ mars_rover/mission_control.rb
@@ -6,14 +6,18 @@ attr_accessor :x, :y, :direction, :position
 	# puts "Line 1 ---> Enter two numbers for the plateau size (ex. 5 5):"
 	# @grid = gets.chomp.gsub(" ", "").split("")
 	def input
-		puts "Line 2 ---> Enter the starting position and direction for Rover 1(ex. 1 2 n):"
+		
+		puts "Please Enter Capital Letters"
+		puts "Enter the starting position and direction for Rover 1(ex. 1 2 n):"
 		@position  = gets.chomp.gsub(" ", "").split("")
 		@x = @position[0].to_i
 		@y = @position[1].to_i
 		@direction =  @position[2]
 
-		puts "Line 3 ---> Enter a series of Move and Turn instructions for Rover 1(ex. lmlmlmlmm):"
-		@user_commands = gets.chomp.split("")
+
+		puts "Enter a series of Move and Turn instructions for Rover 1(ex. lmlmlmlmm):"
+		@user_commands = gets.chomp.gsub(" ", "").split("")
+
 
 		# puts "Line 4 ---> Enter the starting position and direction for Rover 2 (ex. 3 3 e):"
 		# @position = gets.chomp.gsub(" ", "").split("")
@@ -28,17 +32,14 @@ attr_accessor :x, :y, :direction, :position
 	end
 
 
-
-
-
-
-def create_rover
-	@rover = Rover.new(@x, @y, @direction)
-	@rover.read_instruction(@user_commands)
-	@rover.output
-end
+	def create_rover
+		@rover = Rover.new(@x, @y, @direction)
+		@rover.read_instruction(@user_commands)
+		puts @rover.output
+	end
 
 end #end of mission control class
+
 
 ####new mission control
 MissionControl.new.input
--- mars_rover/rover.rb
+++ mars_rover/rover.rb
@@ -1,55 +1,55 @@
 class Rover
-	#attr_accessor :rover_name, :x, :y, :direction, :instructions
+	attr_accessor :instruction
 
 
 	def initialize(x, y, direction)
-		@x = x.to_i
-		@y = y.to_i
-		@direction = direction.to_sym
+		@x = x
+		@y = y
+		@direction = direction
 	end
 
 	def read_instruction(instruction)
 		instruction.each do |command|
-			case @command
-				when "m" then move
-				when "l" then turn_left
-				when "r" then turn_right
+			case command
+			when "M" then move
+			when "L" then turn_left
+			when "R" then turn_right
 			end #End of read_instruction case
 		end # End of each do
 	end # End of read_instruction method
 
 	def turn_left
-		case @direction ####what is this varilable????
-			when :N then @direction = :W
-			when :E then @direction = :N
-			when :S then @direction = :E
-			when :N then @direction = :W
-			else "Enter l or r"
+		case @direction
+			when "N" then @direction = "W"
+			when "E" then @direction = "N"
+			when "S" then @direction = "E"
+			when "W" then @direction = "S"
+				#else "enter l or r"
 		end #### End of turn left method
 	end
 
 	def turn_right
-			case @turn   ####what is this varilable????
-				when :N then @direction = :E
-				when :E then @direction = :S
-				when :S then @direction = :W
-				when :N then @direction = :E
-				else "Enter l or r"
+			case @direction
+				when "N" then @direction = "E"
+				when "E" then @direction = "S"
+				when "S" then @direction = "W"
+				when "W" then @direction = "N"
+				#else "Enter l or r"
 			end #### End of turn right case
 	end ###End of Turn right method
 
 
 	def move
 		case @direction
-			when :N then @y += 1
-			when :E then @x += 1
-			when :S then @y -= 1
-			when :W then @x -= 1
+		when "N" then @y += 1
+		when "E" then @x += 1
+		when "S" then @y -= 1
+		when "W" then @x -= 1
 		end #End of move case
 	end #End of move method
 
 	def output
-		puts "Final Position Rover1: #{@x}, #{@y}, #{@direction}"
+		"#{@x}, #{@y}, #{@direction}"
 	end
 
 end #End of Rover class
